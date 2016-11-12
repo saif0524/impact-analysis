d9334bdbbb896962dae8a3095e4e441484d56118
saif-laptop\Saif <sumahmud33@gmaill.com>
2016-11-12 16:31:02 +0600

    listed down the method calls

--- src/ia/commithistory/CommitParser.java
+++ src/ia/commithistory/CommitParser.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.TimeZone;
 
 import javax.swing.JFileChooser;
@@ -15,6 +16,7 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.DiffFormatter;
+import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.PersonIdent;
@@ -31,184 +33,106 @@ import org.eclipse.jgit.util.io.DisabledOutputStream;
 public class CommitParser {
 
 	public static void main(String[] args) throws IOException, GitAPIException {
-		
-		
-		
+
 		JFileChooser chooser = new JFileChooser();
 		chooser.setCurrentDirectory(new java.io.File("."));
 		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		int returnVal = chooser.showOpenDialog(null);
-		if(returnVal == JFileChooser.APPROVE_OPTION) {
-			System.out.println("You chose to open this file: " +
-					chooser.getSelectedFile().getName());
-			
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			System.out.println("You chose to open this file: "
+					+ chooser.getSelectedFile().getName());
+
 		}
-		
-		
+
 		Repository repo = new FileRepository(chooser.getSelectedFile());
 		Git git = new Git(repo);
-		RevWalk walk = new RevWalk(repo);
+//		RevWalk walk = new RevWalk(repo);
+
+		FileDependencyImpl fileDependency = new FileDependencyImpl();
+
+		List<File> fileList = new ArrayList<File>();
+		ObjectId currentHead = repo.resolve("HEAD^{tree}");
+		
+		System.out.println(currentHead);
+		ObjectId oldHead = repo.resolve("HEAD~7^{tree}");
+		System.out.println(oldHead.getName());
+
 
 		
+		ObjectReader headReader = repo.newObjectReader();
 		
-		List<Ref> branches = git.branchList().call();
+		CanonicalTreeParser oldTreeIter = new CanonicalTreeParser();
+		oldTreeIter.reset(headReader, oldHead);
 
-		for (Ref branch : branches) {
-			String branchName = branch.getName();
+		CanonicalTreeParser newTreeIter = new CanonicalTreeParser();
+		
+		newTreeIter.reset(headReader, currentHead);
+		
+		List<DiffEntry> diffEntries = git.diff().setNewTree(newTreeIter)
+				.setOldTree(oldTreeIter).call();
 
-			System.out.println("Commits of branch: " + branch.getName());
-			System.out.println("-------------------------------------");
-
-			Iterable<RevCommit> commits = git.log().all().call();
-
-			for (RevCommit commit : commits) {
-				boolean foundInThisBranch = false;
-
-				RevCommit targetCommit = walk.parseCommit(repo.resolve(commit.getName()));
-				/*for (Map.Entry<String, Ref> e : repo.getAllRefs().entrySet()) {
-					if (e.getKey().startsWith(Constants.R_HEADS)) {
-						if (walk.isMergedInto(targetCommit, walk.parseCommit(e.getValue().getObjectId()))) {
-							String foundInBranch = e.getValue().getName();
-							if (branchName.equals(foundInBranch)) {
-								foundInThisBranch = true;
-								break;
-							}
-						}
-					}
-				}*/
-
-				
-				
-			
-//				 byte[] commitInfoBytes = targetCommit.getRawBuffer();
-
-				FileDependencyImpl fileDependency = new FileDependencyImpl();
-				
-				List<File> fileList = new ArrayList<File>();
-				ObjectId currentHead = repo.resolve("HEAD^{tree}");
-				ObjectId oldHead = repo.resolve("HEAD~7^{tree}");
-				
-				ObjectReader reader = repo.newObjectReader();
-				CanonicalTreeParser oldTreeIter = new CanonicalTreeParser();
-				oldTreeIter.reset(reader, oldHead);
-				CanonicalTreeParser newTreeIter = new CanonicalTreeParser();
-				newTreeIter.reset(reader, currentHead);
-				List<DiffEntry> diffs= git.diff()
-						.setNewTree(newTreeIter)
-						.setOldTree(oldTreeIter)
-						.call();
-				
-				for(DiffEntry dif: diffs){
-					System.out.println(dif.getNewPath());
-		    		if(dif.getNewPath().contains(".java")){
-						File file = new File(dif.getNewPath());
-			    		fileList.add(file);
-		    		}
-				}
-
-				fileDependency.getFileDependency(fileList);
-				
-/*Parse commit				
-				
-//				http://stackoverflow.com/questions/19941597/jgit-use-treewalk-to-list-files-and-folders
-				RevTree tree = commit.getTree();
-				
-				TreeWalk treeWalk = new TreeWalk(repo);
-				treeWalk.addTree(tree);
-				treeWalk.setRecursive(false);
-				while (treeWalk.next()) {
-				    if (treeWalk.isSubtree()) {
-//				        System.out.println("dir: " + treeWalk.getPathString());
-				        treeWalk.enterSubtree();
-				    } else {
-				        System.out.println("file: " + treeWalk.getPathString());				    	if(treeWalk.getPathString().contains(".java"))
-				    	{   
-				    		File file = new File(treeWalk.getPathString());
-				    		fileList.add(file);
-				    	}				    	
-				
-			
-				    }
-				}
- */				        
-//				fileDependency.setfileList(fileList);
-
-/*				for(File file: fileList){
-					System.out.println(file.getName());
-				}
-*/				
-				
-				
-				
-				
-				
-				
-				
-//				 System.out.println("*******************************");
-//				 String commitInfo = new String(commitInfoBytes);
-//				 
-//				 System.out.println("Commit's Text: \n" + commitInfo );
-//
-//				 
-//				 String treeInfo= new String(treeInfoBytes);
-//				 
-//				 
-//				 System.out.println("Commit's Tree: \n" + treeInfo );
-//				
-				 
-				System.out.println("*******************************");
-				System.out.println("*******************************");
-
-				
-/*				
-				if (foundInThisBranch) {
-					PersonIdent authorIdent = commit.getAuthorIdent();
-					Date authorDate = authorIdent.getWhen();
-					TimeZone authorTimeZone = authorIdent.getTimeZone();
-
-					PersonIdent committerIdent = commit.getCommitterIdent();
-					
-					System.out.println("Info");
-					System.out.println("Commit name/ID: " + commit.getName());
-//					System.out.println("Commited by (uname): "+commit.getAuthorIdent().getName());
-//					System.out.println("Commited Date: " + new Date(commit.getCommitTime()));
-//					System.out.println("Commited Message: "+ commit.getFullMessage());
-//					System.out.println("Time zone: " + authorTimeZone);
-					System.out.println("Date : " + authorDate);
-//					System.out.println("Commiter: " + committerIdent);
-					
-					
-				}
- */
+		for (DiffEntry entry : diffEntries) {
+			System.out.println(entry.getNewPath());
+			if (entry.getNewPath().contains(".java")) {
+				File file = new File(entry.getNewPath());
+				fileList.add(file);
 			}
 		}
 
-		
-		
-		
-		
-		/*
-		ObjectReader reader = git.getRepository().newObjectReader();
-		CanonicalTreeParser oldTreeIter = new CanonicalTreeParser();
-		ObjectId oldTree = git.getRepository().resolve( "HEAD^{tree}" );
-		oldTreeIter.reset( reader, oldTree );
-		CanonicalTreeParser newTreeIter = new CanonicalTreeParser();
-		ObjectId newTree = git.getRepository().resolve( "HEAD~1^{tree}" );
-		newTreeIter.reset( reader, newTree );
+		fileDependency.getFileDependency(fileList);
 
-		DiffFormatter diffFormatter = new DiffFormatter( DisabledOutputStream.INSTANCE );
-		diffFormatter.setRepository( git.getRepository() );
-		List<DiffEntry> entries = diffFormatter.scan( oldTreeIter, newTreeIter );
+		/****************
+		 * Parse commit of all the branches 
+		 * List<Ref> branches =
+		 * git.branchList().call(); for (Ref branch : branches) { String
+		 * branchName = branch.getName();
+		 * 
+		 * System.out.println("Commits of branch: " + branch.getName());
+		 * System.out.println("-------------------------------------");
+		 * 
+		 * /************ Iterable<RevCommit> commits = git.log().all().call();
+		 * 
+		 * 
+		 * Parse commit 
+		 * for (RevCommit commit : commits) { boolean
+		 * foundInThisBranch = false;
+		 * 
+		 * RevCommit targetCommit = walk.parseCommit(repo.resolve(commit
+		 * .getName()));
+		 * 
+		 * 
+		 * // http://stackoverflow.com/questions/19941597/jgit-use- treewalk
+		 * -to-list-files-and-folders RevTree tree = commit.getTree();
+		 * 
+		 * TreeWalk treeWalk = new TreeWalk(repo); treeWalk.addTree(tree);
+		 * treeWalk.setRecursive(false); while (treeWalk.next()) { if
+		 * (treeWalk.isSubtree()) { // System.out.println("dir: " +
+		 * treeWalk.getPathString()); treeWalk.enterSubtree(); } else {
+		 * System.out.println("file: " + treeWalk.getPathString());
+		 * if(treeWalk.getPathString().contains(".java")) { File file = new
+		 * File(treeWalk.getPathString()); fileList.add(file); }
+		 * 
+		 * 
+		 * } }
+		 ************/
 
-		for( DiffEntry entry : entries ) {
-		  System.out.println( entry.getChangeType() );
-		}
-		
-		
-		*/
-		
-	
-		
- 	
+		/***
+		 * for (Map.Entry<String, Ref> e : repo.getAllRefs().entrySet()) { if
+		 * (e.getKey().startsWith(Constants.R_HEADS)) { if
+		 * (walk.isMergedInto(targetCommit,
+		 * walk.parseCommit(e.getValue().getObjectId()))) { String foundInBranch
+		 * = e.getValue().getName(); if (branchName.equals(foundInBranch)) {
+		 * foundInThisBranch = true; break; } } } }
+		 * 
+		 * Branch Info if (foundInThisBranch) {
+		 * 
+		 * PersonIdent authorIdent = commit.getAuthorIdent(); Date authorDate =
+		 * authorIdent.getWhen(); PersonIdent committerIdent =
+		 * commit.getCommitterIdent();
+		 * 
+		 * System.out.println("Info"); System.out.println("Commit name/ID: " +
+		 * commit.getName()); System.out.println("Date : " + authorDate);
+		 * System.out.println("Commiter: " + committerIdent); } } }
+		 *****************************/
 	}
 }
--- src/ia/filedependency/FileDependencyImpl.java
+++ src/ia/filedependency/FileDependencyImpl.java
@@ -1,15 +1,21 @@
 package ia.filedependency;
 
 import japa.parser.JavaParser;
+import japa.parser.ParseException;
 import japa.parser.ast.CompilationUnit;
 import japa.parser.ast.body.MethodDeclaration;
+import japa.parser.ast.expr.BinaryExpr;
+import japa.parser.ast.expr.Expression;
+import japa.parser.ast.expr.MethodCallExpr;
 import japa.parser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -35,14 +41,40 @@ public class FileDependencyImpl implements IFileDependency {
 			System.out.println("Methodlist: ");
 			System.out.println(this.getMethodList());
 
+
+			try {
+				getFunctionCalls(file);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
 		}
-//		for (File file : fileList) {
-//			System.out.println("Readnig file: " + file.getName());
-//			getDpendency(file);
-//		}
+		// for (File file : fileList) {
+		// System.out.println("Readnig file: " + file.getName());
+		// getDpendency(file);
+		// }
 
 	}
 
+	private void getFunctionCalls(File file) throws ParseException, IOException{
+		 FileInputStream inputFile = new FileInputStream(file);
+
+	        CompilationUnit compilationUnit;
+	        try
+	        {
+	        	compilationUnit = JavaParser.parse(inputFile);
+	        }
+	        finally
+	        {
+	            inputFile.close();
+	        }
+
+	        new MethodCallVisitor ().visit(compilationUnit, null);
+	}
+	
+	
 	private void getDpendency(File file) {
 		BufferedReader bufferedReader = null;
 		try {
@@ -67,7 +99,7 @@ public class FileDependencyImpl implements IFileDependency {
 
 	private void getListOfAllMethods(File javaFile) {
 		CompilationUnit compileUnit = getMethods(javaFile);
-		MethodVisitor visitor = new MethodVisitor();
+		MethodDeclarationVisitor visitor = new MethodDeclarationVisitor();
 		visitor.visit(compileUnit, null);
 
 	}
@@ -78,7 +110,7 @@ public class FileDependencyImpl implements IFileDependency {
 			compileUnit = JavaParser.parse(javaFile);
 		} catch (japa.parser.ParseException e) {
 			e.printStackTrace();
-		}catch(IOException e){
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		return compileUnit;
@@ -92,28 +124,52 @@ public class FileDependencyImpl implements IFileDependency {
 		this.methodList = methodList;
 	}
 
-	private class MethodVisitor extends VoidVisitorAdapter {
+	
+	
+	private class MethodDeclarationVisitor extends VoidVisitorAdapter {
+		@Override
 		public void visit(MethodDeclaration method, Object arg) {
 			methodList.add(method.getName());
 
 		}
 	}
+	
+	
+	private class MethodCallVisitor extends VoidVisitorAdapter {
+		@Override
+		public void visit(MethodCallExpr methodCall, Object arg) {
+			System.out.print("Method call: " + methodCall.getName() + "\n");
+			List<Expression> args = methodCall.getArgs();
+			if (args != null)
+				handleExpressions(args);
+		}
 
-	public static void main(String[] args){
-		
+		private void handleExpressions(List<Expression> expressions) {
+			for (Expression expr : expressions) {
+				if (expr instanceof MethodCallExpr)
+					visit((MethodCallExpr) expr, null);
+				else if (expr instanceof BinaryExpr) {
+					BinaryExpr binExpr = (BinaryExpr) expr;
+					handleExpressions(Arrays.asList(binExpr.getLeft(),
+							binExpr.getRight()));
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
 		FileDependencyImpl fileDependency = new FileDependencyImpl();
-		
+
 		List<File> fileList = new ArrayList<File>();
-		
-		File file1 = new File("D:/Academics/8th_Semester/801 Project/Workspace/java-blog-aggregator/src/main/java/cz/jiripinkas/jba/controller/AdminController.java");
-		File file2 = new File("D:/Academics/8th_Semester/801 Project/Workspace/java-blog-aggregator/src/main/java/cz/jiripinkas/jba/controller/RegisterController.java");
+
+		File file1 = new File(
+				"D:/Academics/8th_Semester/801 Project/Workspace/java-blog-aggregator/src/main/java/cz/jiripinkas/jba/controller/AdminController.java");
+		File file2 = new File(
+				"D:/Academics/8th_Semester/801 Project/Workspace/java-blog-aggregator/src/main/java/cz/jiripinkas/jba/controller/RegisterController.java");
 		fileList.add(file1);
 		fileList.add(file2);
-		
-		
-//		fileDependency.setfileList(fileList);
-		
+
 		fileDependency.getFileDependency(fileList);
 	}
 }
-
