9106550c27487b2c0843f1367af306b004474f8b
saif-laptop\Saif <sumahmud33@gmaill.com>
2018-01-30 01:00:00 +0600

    written a helper method for extracting the actual method signature

--- src/ia/changecollector/MethodFinder.java
+++ src/ia/changecollector/MethodFinder.java
@@ -5,55 +5,36 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
-import java.util.ListIterator;
 
 public class MethodFinder {
-
-	private File inputFile;
-
 	
-	public static void readFile(String fileName) {
-		
-		try {
-			BufferedReader bReader = new BufferedReader(new FileReader(fileName));
-			String line = bReader.readLine();
-			while (bReader.readLine() != null) {
-				line = bReader.readLine();
-				System.out.println(line);
-			}
-		} catch (FileNotFoundException e ){
-		
-			e.printStackTrace();
-		} catch (IOException e)  {
-			e.printStackTrace();
-		}
-	}
-	public static void main(String args[]) {
-		
-		//readFile("./tt/changLog.txt");		
-
-		//found a regex from https://stackoverflow.com/questions/68633/regex-that-will-match-a-java-method-declaration;
-		String pattern = ".[\\-\\+][ \\t]*(?:(?:public|protected|private)\\s+)?(?:(static|final|native|synchronized|abstract|threadsafe|transient|(?:<[?\\w\\[\\] ,&]+>)|(?:<[^<]*<[?\\w\\[\\] ,&]+>[^>]*>)|(?:<[^<]*<[^<]*<[?\\w\\[\\] ,&]+>[^>]*>[^>]*>))\\s+){0,}(?!return)\\b([\\w.]+)\\b(?:|(?:<[?\\w\\[\\] ,&]+>)|(?:<[^<]*<[?\\w\\[\\] ,&]+>[^>]*>)|(?:<[^<]*<[^<]*<[?\\w\\[\\] ,&]+>[^>]*>[^>]*>))((?:\\[\\]){0,})\\s+\\b\\w+\\b\\s*\\(\\s*(?:\\b([\\w.]+)\\b(?:|(?:<[?\\w\\[\\] ,&]+>)|(?:<[^<]*<[?\\w\\[\\] ,&]+>[^>]*>)|(?:<[^<]*<[^<]*<[?\\w\\[\\] ,&]+>[^>]*>[^>]*>))((?:\\[\\]){0,})(\\.\\.\\.)?\\s+(\\w+)\\b(?![>\\[])\\s*(?:,\\s+\\b([\\w.]+)\\b(?:|(?:<[?\\w\\[\\] ,&]+>)|(?:<[^<]*<[?\\w\\[\\] ,&]+>[^>]*>)|(?:<[^<]*<[^<]*<[?\\w\\[\\] ,&]+>[^>]*>[^>]*>))((?:\\[\\]){0,})(\\.\\.\\.)?\\s+(\\w+)\\b(?![>\\[])\\s*){0,})?\\s*\\)(?:\\s*throws [\\w.]+(\\s*,\\s*[\\w.]+))?\\s*(?:\\{|;)[ \\t]*$";
-		
-		
-		String str = "+    public String getLookAndFeelFromName(String name) {";
-		
+	private File inputFile;
+	
+	/** provides a list of methods (in pairs) that has changes in signatures**/
+	public List<String> getMethodSignatureChangeLog(String fileName) {
+		//Used a regex from https://stackoverflow.com/questions/68633/regex-that-will-match-a-java-method-declaration;		
+		String pattern = ".[\\-\\+][ \\t]*(?:(?:public|protected|private)\\s+)?"+
+				"(?:(static|final|native|synchronized|abstract|threadsafe|transient|"+
+				"(?:<[?\\w\\[\\] ,&]+>)|(?:<[^<]*<[?\\w\\[\\] ,&]+>[^>]*>)|(?:<[^<]*<["+
+				"^<]*<[?\\w\\[\\] ,&]+>[^>]*>[^>]*>))\\s+){0,}(?!return)\\b([\\w.]+)\\b"+
+				"(?:|(?:<[?\\w\\[\\] ,&]+>)|(?:<[^<]*<[?\\w\\[\\] ,&]+>[^>]*>)|(?:<[^<]*"+
+				"<[^<]*<[?\\w\\[\\] ,&]+>[^>]*>[^>]*>))((?:\\[\\]){0,})\\s+\\b\\w+"+
+				"\\b\\s*\\(\\s*(?:\\b([\\w.]+)\\b(?:|(?:<[?\\w\\[\\] ,&]+>)|(?:<[^<]*<[?\\w\\[\\] ,&]+"+
+				">[^>]*>)|(?:<[^<]*<[^<]*<[?\\w\\[\\] ,&]+>[^>]*>[^>]*>))((?:\\[\\]){0,})(\\.\\.\\.)?\\s+"+
+				"(\\w+)\\b(?![>\\[])\\s*(?:,\\s+\\b([\\w.]+)\\b(?:|(?:<[?\\w\\[\\] ,&]+>)|(?:<[^<]*<[?\\w\\[\\] ,&]+"+
+				">[^>]*>)|(?:<[^<]*<[^<]*<[?\\w\\[\\] ,&]+>[^>]*>[^>]*>))((?:\\[\\]){0,})(\\.\\.\\.)?\\s+"+
+				"(\\w+)\\b(?![>\\[])\\s*){0,})?\\s*\\)(?:\\s*throws [\\w.]+(\\s*,\\s*[\\w.]+))?\\s*(?:\\{|;)[ \\t]*$";
 		
 		List<String> methodSignatureList = new ArrayList<String>();
-			
 		
 		try {
 			BufferedReader bReader = new BufferedReader(new FileReader("./esxternaltestfiles/changeLog.txt"));
-			String line;// = bReader.readLine();
+			String line = "";
 			
 			while ((line = bReader.readLine()) != null) {
 				if(line.matches(pattern)) {
-					System.out.println(line);
 					methodSignatureList.add(line);
 				}
 			}
@@ -64,17 +45,16 @@ public class MethodFinder {
 			e.printStackTrace();
 		}
 		
-		for(int i=0; i<methodSignatureList.size(); i++) {
-			String method1 = methodSignatureList.get(i);
-			String method2 = methodSignatureList.get(++i);
-			
-			System.out.println(method1.substring(3, method1.length()-1));
-			System.out.println(method2.substring(3, method2.length()-1));
-			
-//			System.out.println(method1 + "\t\t" + method2);
-		}
-	}
+		return methodSignatureList;
+	}//end of getMethodSignatureChangeLog
 	
+	
+	/**helper method for subtraction of plus or minus symbol 
+	 * and tab from the starting of a line
+	 * */
+	private String extractMethodSignature(String methodSignature) {
+		return methodSignature.substring(3, methodSignature.length()-1);
+	}
 	
 	public File getInputFile() {
 		return inputFile;
@@ -85,4 +65,26 @@ public class MethodFinder {
 	}
 
 	
+	/** Constructor of Test***/
+	public MethodFinder() {
+		
+		List<String> methodSignatureList = new ArrayList<String>();
+		
+		methodSignatureList = getMethodSignatureChangeLog("./esxternaltestfiles/changeLog.txt");
+		
+		for(int i=0; i<methodSignatureList.size(); i++) {
+			
+			String method1 = extractMethodSignature(methodSignatureList.get(i));
+			String method2 = extractMethodSignature(methodSignatureList.get(++i));
+			
+			System.out.println(method1);
+			System.out.println(method2);
+			
+		}
+	}
+	
+	/***Main method for testing***/
+	public static void main(String args[]) {
+		new MethodFinder();
+	}
 }
